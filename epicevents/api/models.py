911ae5c169453ddd7fe071aa630b249d5fdb7f2f
Julien Couignoux <j.couignoux@gmail.com>
2022-08-24 17:07:01 +0200

    test user_model

--- epicevents/api/models.py
+++ epicevents/api/models.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Client(models.Model):
