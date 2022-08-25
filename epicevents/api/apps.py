5ddf28d45626e7dfc6ea02814167d05b82465c24
Julien Couignoux <j.couignoux@gmail.com>
2022-08-25 18:34:09 +0200

    add auto creation groups

--- epicevents/api/apps.py
+++ epicevents/api/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        from django.contrib.auth.models import Group
+
+        Group.objects.get_or_create(name='Management')
+        Group.objects.get_or_create(name='Sales')
+        Group.objects.get_or_create(name='Support')
