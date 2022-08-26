c59417301faa66c2033b524e0638689b65500032
Julien Couignoux <j.couignoux@gmail.com>
2022-08-26 10:46:26 +0200

    update admin

--- epicevents/api/admin.py
+++ epicevents/api/admin.py
@@ -7,14 +7,10 @@ from api.models import Client, Contract, Event, EventStatus
 # Register your models here.
 
 
-# class GroupInline(admin.TabularInline):
-#     model = User.groups.through
-#     extra = 0
-
-
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'last_name', 'first_name',
                     'email', 'is_staff', 'group_name')
+    search_fields = ['username']
 
     def group_name(self, obj):
         return obj.groups.values_list('name', flat=True).get()
@@ -30,24 +26,25 @@ admin.site.register(User, UserAdmin)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ['company_name', 'last_name', 'first_name', 'email', 'phone', 'mobil',
                     'date_created', 'date_updated', 'sales_contact', 'status']
-    list_filter = ('last_name',)
-    search_fields = ['last_name']
+    search_fields = ['last_name', 'email']
 
 
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
-    list_display = ['client', 'sales_contact', 'date_created',
+    list_display = ['client', 'id', 'sales_contact', 'date_created',
                     'date_updated', 'status', 'amount', 'payment_due']
-    list_filter = ('sales_contact',)
-    search_fields = ['sales_contact']
+    search_fields = ['client', 'client__email', 'date_created', 'amount']
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['contract_id', 'event_status', 'date_created', 'date_updated',
+    list_display = ['client', 'contract_id', 'event_status', 'date_created', 'date_updated',
                     'support_contact', 'attendees', 'event_date', 'notes']
-    list_filter = ('contract_id',)
-    search_fields = ['contract_id']
+    search_fields = ['contract_id__client__last_name',
+                     'contract_id__client__email', 'event_date']
+
+    def client(self, obj):
+        return obj.contract_id.client
 
 
 @admin.register(EventStatus)
