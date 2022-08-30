37adeba720c6d1a264faf1e34f4bc5dd358a2b34
Julien Couignoux <j.couignoux@gmail.com>
2022-08-30 10:49:55 +0200

    add user create

--- epicevents/staff/views.py
+++ epicevents/staff/views.py
@@ -1,11 +1,15 @@
 
 from rest_framework.viewsets import ModelViewSet
+from django.db import IntegrityError, transaction
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from rest_framework import status
 
 from staff.models import User
 from staff.serialisers import UserListSerializer, UserDetailSerializer
 from staff.permissions import MemberPermission
+
 
 # Create your views here.
 
@@ -33,3 +37,14 @@ class UserViewset(MultipleSerializerMixin, ModelViewSet):
     def get_queryset(self):
 
         return User.objects.filter(is_superuser=False)
+
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save(is_staff=True)
+
+        return Response({
+            'project': UserListSerializer(user, context=self.get_serializer_context()).data,
+            'message': "User created successfully."},
+            status=status.HTTP_201_CREATED)
