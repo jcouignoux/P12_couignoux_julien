7a72b5829b96e73828db608be50c7059a37ddadf
Julien Couignoux <j.couignoux@gmail.com>
2022-09-08 16:00:00 +0200

    add readonly_fields for manager

--- epicevents/events/admin.py
+++ epicevents/events/admin.py
@@ -17,6 +17,14 @@ class EventAdmin(admin.ModelAdmin):
     def contract(self, obj):
         return obj.contract
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj is not None:
+            if request.user.role == 'MA':
+                return self.readonly_fields + ('client', 'contract', 'event_status', 'date_created',
+                                               'date_updated', 'attendees', 'event_date', 'notes')
+
+        return self.readonly_fields
+
 
 @admin.register(EventStatus)
 class EventStatusAdmin(admin.ModelAdmin):
